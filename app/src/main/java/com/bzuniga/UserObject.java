ff94b10dbdf444da4a36c2703008ec653f7ce6b2
Bryan Zúñiga <bryan.zuniga@udla.edu.ec>
2020-04-06 17:41:19 -0500

    Display User's Name

--- app/src/main/java/com/bzuniga/UserObject.java
+++ app/src/main/java/com/bzuniga/UserObject.java
@@ -13,6 +13,10 @@ public class UserObject {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getPhone() {
         return phone;
     }
